fad247f0d7a0f8240d5f57280c9dae466a353193
Sebastian Stenbom <ryezze@gmail.com>
2022-01-06 12:39:12 +0100

    added javascript

---
+++ Leibniz.js
@@ -0,0 +1,13 @@
+let pi = 0;
+
+for (let i = 0; i < 1000000000; i++)
+{
+    let variabelpart = 0;
+    for(let j =1 ; j <= i; j++)
+    {
+        variabelpart += (Math.pow(-1, j+1) / ((2 * j) - 1));
+    }
+
+    pi = 4 * variabelpart;
+    console.log("Pi: " + pi);
+}
