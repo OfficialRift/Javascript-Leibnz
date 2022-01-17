e6177ba5fbe637babaa84848a4b441001da4f390
Sebastian Stenbom <ryezze@gmail.com>
2022-01-17 18:44:59 +0100

    fix loop

--- Leibniz.js
+++ Leibniz.js
@@ -1,13 +1,9 @@
 let pi = 0;
+let variabelpart = 0;
 
-for (let i = 0; i < 100000; i++)
+for (let i = 1; i < 100001; i++)
 {
-    let variabelpart = 0;
-    for(let j =1 ; j <= i; j++)
-    {
-        variabelpart += (Math.pow(-1, j+1) / ((2 * j) - 1));
-    }
-
+    variabelpart += (Math.pow(-1, i+1) / ((2 * i) - 1));
     pi = 4 * variabelpart;
     console.log("Pi: " + pi);
 }
